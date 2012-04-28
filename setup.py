a914cdf40a2fe5e1f9b4e27895c7aa9ce2f5c119
David Miller <david@deadpansincerity.com>
2012-04-28 14:25:59 +0100

    Update setup.py trove classifiers now that we run on Py3k

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 3.2",
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
